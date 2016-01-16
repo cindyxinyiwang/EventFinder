1d6a06f2a8bfb9040791c90453693f2b96da0433
cindyxinyiwang <xwang29@nd.edu>
2016-01-16 17:42:33 -0500

    explore view

--- Event-Finder/FirstViewController.m
+++ Event-Finder/FirstViewController.m
@@ -14,14 +14,55 @@
 
 @implementation FirstViewController
 
+@synthesize mapView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.locationManger = [[CLLocationManager alloc] init];
+    #ifdef __IPHONE_8_0
+    if(IS_OS_8_OR_LATER) {
+        [self.locationManger requestWhenInUseAuthorization];
+    }
+    #endif
+    [self.locationManger startUpdatingLocation];
+    
+    self.mapView.showsUserLocation = YES;
+    self.mapView.delegate = self;
+    
+    [self loadEventsOnMap];
+}
+
+- (void)loadEventsOnMap {
+    NSString *location = @"450 Serra Mall, Stanford, CA 94305";
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder geocodeAddressString:location
+                 completionHandler:^(NSArray* placemarks, NSError* error){
+                     if (placemarks && placemarks.count > 0) {
+                         CLPlacemark *topResult = [placemarks objectAtIndex:0];
+                         MKPlacemark *placemark = [[MKPlacemark alloc] initWithPlacemark:topResult];
+                         
+                         MKCoordinateRegion region = self.mapView.region;
+                         region.center = placemark.region.center;
+                         region.span.longitudeDelta /= 8.0;
+                         region.span.latitudeDelta /= 8.0;
+                         
+                         [self.mapView setRegion:region animated:YES];
+                         [self.mapView addAnnotation:placemark];
+                     }
+                 }
+     ];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
 }
 
 @end
